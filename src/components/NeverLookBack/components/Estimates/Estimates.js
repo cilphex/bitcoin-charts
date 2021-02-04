2578bb198e5e24c1a77c7e722ee341b8d8056e8c
Craig Hammell <cilphex@gmail.com>
2021-02-04 14:47:23 -0800

    move some estimates number crunching into a store?

--- src/components/NeverLookBack/components/Estimates/Estimates.js
+++ src/components/NeverLookBack/components/Estimates/Estimates.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { observer } from 'mobx-react';
 import moment from 'moment';
 import { moneyFormat } from 'lib/utils.js';
 
+import EstimatesStore from './EstimatesStore.js';
 import Chart from 'components/Chart';
 import styles from "./Estimates.scss"
 
@@ -10,73 +11,17 @@ import styles from "./Estimates.scss"
 class Estimates extends Chart {
   constructor(props) {
     super(props);
-  }
 
-  get today() {
-    if (this._today) {
-      return this._today;
-    }
-
-    const { chartData } = this.dataStore;
-    const { regressionData, standardDeviationNlb } = chartData;
-
-    const todayData = regressionData.find(i =>
-      moment(i.date).isSame(moment(), 'day')
-    )
-
-    const { regressionNlb } = todayData;
-    const expected = Math.round(Math.pow(10, regressionNlb))
-    const min = Math.round(Math.pow(10, regressionNlb - standardDeviationNlb))
-    const max = Math.round(Math.pow(10, regressionNlb + standardDeviationNlb))
-
-    this._today = {
-      expected,
-      min,
-      max
-    }
-
-    return this._today;
-  }
-
-  get years() {
-    if (this._years) {
-      return this._years;
-    }
-
-    const { chartData } = this.dataStore;
-    const { regressionData } = chartData;
-
-    this._years = Array(5).fill(null)
-      .map((item, i) => moment().year() + i)
-      .map((year, i) =>
-        regressionData.find(dataItem =>
-          moment(dataItem.date).isSame(moment(`${year}-01-01`), 'day')
-        )
-      )
-
-    return this._years;
-  }
-
-  get magnitudes() {
-    if (this._magnitudes) {
-      return this._magnitudes;
-    }
-
-    const { chartData } = this.dataStore;
-    const { regressionData } = chartData;
-
-    this._magnitudes = Array(5).fill(null)
-      .map((val, i) => Math.pow(10, i+3)) // 10,000 to 100,000,000
-      .map((price, i) =>
-          regressionData.find(dataItem =>
-              Math.pow(10, dataItem.regressionNlb) > price
-          )
-      )
-
-    return this._magnitudes;
+    this.estimatesStore = new EstimatesStore(this.dataStore.chartData);
   }
 
   get chartView() {
+    const { ready, today, years, magnitudes } = this.estimatesStore;
+
+    if (!ready) {
+      return <div className={styles.readying}>Readying up...</div>;
+    }
+
     return (
       <div className={styles.estimates}>
         <div>
@@ -85,15 +30,15 @@ class Estimates extends Chart {
             <tbody>
               <tr className={styles.deviation}>
                 <td>Max price</td>
-                <td>{moneyFormat(this.today.max)}</td>
+                <td>{moneyFormat(today.max)}</td>
               </tr>
               <tr className={styles.expected}>
                 <td>Expected price</td>
-                <td>{moneyFormat(this.today.expected)}</td>
+                <td>{moneyFormat(today.expected)}</td>
               </tr>
               <tr className={styles.deviation}>
                 <td>Min price</td>
-                <td>{moneyFormat(this.today.min)}</td>
+                <td>{moneyFormat(today.min)}</td>
               </tr>
             </tbody>
           </table>
@@ -103,7 +48,7 @@ class Estimates extends Chart {
           <h3>5 Years</h3>
           <table>
             <tbody>
-              {this.years.map((year, i) =>
+              {years.map((year, i) =>
                 <tr key={i}>
                   <td>{moment(year.date).year()}</td>
                   <td>{moneyFormat(Math.round(Math.pow(10, year.regressionNlb)))}</td>
@@ -117,7 +62,7 @@ class Estimates extends Chart {
           <h3>Goals</h3>
           <table>
             <tbody>
-              {this.magnitudes.map((magnitude, i) =>
+              {magnitudes.map((magnitude, i) =>
                 <tr key={i}>
                   <td>{moneyFormat(Math.round(Math.pow(10, Math.floor(magnitude.regressionNlb))))}</td>
                   <td>{moment(magnitude.date).format('MMM D, YYYY')}</td>
---
+++ src/components/NeverLookBack/components/Estimates/EstimatesStore.js
@@ -0,0 +1,73 @@
+import { observable, computed } from 'mobx';
+import moment from "moment";
+
+class EstimatesStore {
+  @observable today;
+  @observable years;
+  @observable magnitudes;
+
+  chartData = null;
+
+  constructor(chartData) {
+    this.chartData = chartData;
+    this.processData();
+  }
+
+  @computed get ready() {
+    return !!(this.today && this.years && this.magnitudes);
+  }
+
+  processData = async() => {
+    await new Promise(resolve =>
+      setTimeout(resolve, 0)
+    );
+    this.processToday();
+    this.processYears();
+    this.processMagnitudes();
+  }
+
+  processToday() {
+    const { regressionData, standardDeviationNlb } = this.chartData;
+
+    const todayData = regressionData.find(i =>
+      moment(i.date).isSame(moment(), 'day')
+    )
+
+    const { regressionNlb } = todayData;
+    const expected = Math.round(Math.pow(10, regressionNlb))
+    const min = Math.round(Math.pow(10, regressionNlb - standardDeviationNlb))
+    const max = Math.round(Math.pow(10, regressionNlb + standardDeviationNlb))
+
+    this.today = {
+      expected,
+      min,
+      max
+    };
+  }
+
+  processYears() {
+    const { regressionData } = this.chartData;
+
+    this.years = Array(5).fill(null)
+      .map((item, i) => moment().year() + i)
+      .map((year, i) =>
+        regressionData.find(dataItem =>
+          moment(dataItem.date).isSame(moment(`${year}-01-01`), 'day')
+        )
+      )
+  }
+
+  processMagnitudes() {
+    const { regressionData } = this.chartData;
+
+    this.magnitudes = Array(5).fill(null)
+      .map((val, i) => Math.pow(10, i+3)) // 10,000 to 100,000,000
+      .map((price, i) =>
+        regressionData.find(dataItem =>
+          Math.pow(10, dataItem.regressionNlb) > price
+        )
+      )
+  }
+}
+
+export default EstimatesStore;
