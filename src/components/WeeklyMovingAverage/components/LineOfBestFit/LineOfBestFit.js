ac8debfb7b8119ebb2907fe05bbe6f391a1a8e92
Craig Hammell <cilphex@gmail.com>
2022-05-31 15:28:16 -0500

    start work on horizontal price indicator for chart hovers

--- src/components/WeeklyMovingAverage/components/LineOfBestFit/LineOfBestFit.js
+++ src/components/WeeklyMovingAverage/components/LineOfBestFit/LineOfBestFit.js
@@ -8,7 +8,9 @@ import { scalePower, scaleLog } from '@vx/scale';
 import { LinePath } from '@vx/shape';
 import { AxisBottom, AxisLeft } from '@vx/axis';
 import { localPoint } from '@vx/event';
+import { Text } from '@vx/text'
 import { bisector } from 'd3-array';
+import { moneyFormat } from 'lib/utils';
 
 import Chart from 'components/Chart';
 import chartStyles from 'styles/chart.scss';
@@ -33,13 +35,22 @@ class PowerLawScaleChart extends Chart {
   onMouseMove(e) {
     const { regressionData } = this.dataStore.chartData;
     const { xScale, yScale } = this.scales;
-    const { margin } = this.chartDimensions;
+    const { margin, innerHeight } = this.chartDimensions;
     const point = localPoint(e);
     const x = point.x - margin.left;
+    const y = point.y - margin.top;
     const date = xScale.invert(x);
     const index = Math.round(date)
     const item = regressionData[index];
     const xPos = xScale(index);
+    const yPos = y;
+
+    // Was playing around, now number these
+    const exponentRange = yScale.domain().map(i => Math.log10(i))
+    const percent = (innerHeight - y) / innerHeight
+    const exponentDiff = exponentRange[1] - exponentRange[0]
+    const exponent = percent * exponentDiff + exponentRange[0]
+    const priceLabelValue = moneyFormat(Math.pow(10, exponent))
 
     // Basic chart data does not have regression predictions so item may not exist
     // if we hover past the current day. (Hence "item &&".)
@@ -48,6 +59,8 @@ class PowerLawScaleChart extends Chart {
 
     this.chartStore.setData({
       xPos,
+      priceLabelValue,
+      yPos,
       yPosPrice,
       yPosRegression,
     });
@@ -97,6 +110,8 @@ class PowerLawScaleChart extends Chart {
     const { hoverData } = this.chartStore;
     const {
       xPos,
+      priceLabelValue,
+      yPos,
       yPosPrice,
       yPosRegression,
     } = hoverData;
@@ -143,6 +158,15 @@ class PowerLawScaleChart extends Chart {
 
           { hoverData && (
             <Group>
+              {/* The horizontal line that follows the cursor when hovering */}
+              <line
+                x1={0}
+                y1={yPos}
+                x2={innerWidth}
+                y2={yPos}
+                className={chartStyles.mouseLine}
+              />
+
               {/* The vertical line that follows the cursor when hovering */}
               <line
                 x1={xPos}
@@ -152,6 +176,22 @@ class PowerLawScaleChart extends Chart {
                 className={chartStyles.mouseLine}
               />
 
+              <rect
+                x={10}
+                y={yPos - 10}
+                rx={5}
+                className={chartStyles.priceLabel}
+              />
+
+              {/* Price text */}
+              <Text
+                x={15}
+                y={yPos + 5}
+                className={chartStyles.priceLabelText}
+              >
+                {priceLabelValue}
+              </Text>
+
               { yPosPrice && (
                 <circle
                   cx={xPos}
@@ -160,13 +200,11 @@ class PowerLawScaleChart extends Chart {
                 />
               )}
 
-              { yPosRegression && (
-                <circle
-                  cx={xPos}
-                  cy={yPosRegression}
-                  className={`${chartStyles.mouseCircle} ${chartStyles.mouseCirclePrice}`}
-                />
-              )}
+              <circle
+                cx={xPos}
+                cy={yPosRegression}
+                className={`${chartStyles.mouseCircle} ${chartStyles.mouseCircleForwardMin}`}
+              />
             </Group>
           )}
 
--- src/lib/constants.js
+++ src/lib/constants.js
@@ -2,8 +2,4 @@ export default {
   regressionData: {
     maxDays: 10000, // 10,000
   },
-  regressionChart: {
-    // xMax: 3000,    // Days passed
-    // yMax: 1000000, // Price
-  }
 }
