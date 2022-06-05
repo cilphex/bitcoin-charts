d24ffdb3d282d03ec34cf1bc30a86fc67d5ede96
Craig Hammell <cilphex@gmail.com>
2022-06-05 00:13:50 -0500

    add basic 404 page

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 // Stores
 import DataStore from "stores/DataStore.js";
@@ -27,18 +27,25 @@ class App extends React.Component {
       <Router>
         <Nav dataStore={this.dataStore}/>
         <div className={styles.content}>
-          <Route path="/never-look-back">
-            <NeverLookBack dataStore={this.dataStore}/>
-          </Route>
-          <Route path="/power-law-corridor">
-            <PowerLawCorridor dataStore={this.dataStore}/>
-          </Route>
-          <Route path="/weekly-moving-average">
-            <WeeklyMovingAverage dataStore={this.dataStore}/>
-          </Route>
-          <Route exact path="/">
-            <Home dataStore={this.dataStore}/>
-          </Route>
+          {/* Switch means one route or the other, which lets us
+              have the catch-all route for a 404 page at the bottom */}
+          <Switch>
+            <Route path="/never-look-back">
+              <NeverLookBack dataStore={this.dataStore}/>
+            </Route>
+            <Route path="/power-law-corridor">
+              <PowerLawCorridor dataStore={this.dataStore}/>
+            </Route>
+            <Route path="/weekly-moving-average">
+              <WeeklyMovingAverage dataStore={this.dataStore}/>
+            </Route>
+            <Route exact path="/">
+              <Home dataStore={this.dataStore}/>
+            </Route>
+            <Route exact path="*">
+              This page does not exist.
+            </Route>
+          </Switch>
         </div>
       </Router>
     );
